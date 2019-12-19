e6f9c65963ced22bb4c862ee509232599ba9cb82
httpedro <pedroaugusto.ifal@gmail.com>
2019-12-18 22:44:08 -0200

    criada a classe livro e adicionados os metodos podroes nas duas classes exitentes Livro e Bibliotecario  , metodos getters and setters e concrutor, array de livros criado na classe bibliotecario

--- src/Bibliotecario.java
+++ src/Bibliotecario.java
@@ -1,8 +1,48 @@
+import java.util.ArrayList;
 
 public class Bibliotecario {
 
 	private String nome;
 	private String email;
 	private String senha;
+	ArrayList<Livro> livros;
+	
+	public Bibliotecario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public ArrayList<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(ArrayList<Livro> livros) {
+		this.livros = livros;
+	}
+	
+	
 }
---
+++ src/Livro.java
@@ -0,0 +1,48 @@
+
+public class Livro {
+	private String titulo;
+	private String autor;
+	private int anoLancamento;
+	private long id;
+	
+	public Livro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(int anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+}
